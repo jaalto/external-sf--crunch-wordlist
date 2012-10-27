e65172f992e8eb7f534ee13ce6c323164082796e
Jari Aalto <jari.aalto@cante.net>
2012-10-27 12:31:04 +0300

    crunch.c: gpl 2 or later
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- crunch.c
+++ crunch.c
@@ -1,23 +1,32 @@
 /*  character-set based brute-forcing algorithm
- *  Copyright (C) 2004 by mimayin@aciiid.ath.cx
- *  Copyright (C) 2008, 2009, 2010, 2011, 2012 by bofh28@gmail.com
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, version 2 only of the License
+ *  Copyright
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *      Copyright (C) 2008-2012 Jason <bofh28@gmail.com>
+ *      Copyright (C) 2004 <mimayin@aciiid.ath.cx>
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  License
  *
- *  crunch version 1.0 was written by mimayin@aciiid.ath.cx
- *  all later versions of crunch have been updated by bofh28@gmail.com
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
  *
- *  Changelog:
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Description
+ *
+ *      Crunch version 1.0 was written by <mimayin@aciiid.ath.cx>
+ *      all later versions of crunch have been updated by <bofh28@gmail.com>.
+ *
+ *  Changelog
+ *
  *  version 1.0 initial release in 2004 (guess based on copyright date) 194 lines of code
  *  version 1.1 added support to read character list from a specified file
  *  version 1.2 added output parameter
